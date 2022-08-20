5d889b176b8faa21654af41d0e1f543da9c219df
Emre Küçük <emrekucuk74@hotmail.com>
2022-08-20 17:37:24 +0300

    List-Dictionary: Created New Project as dictionary-example

---
+++ list-dictionary/dictionary-example/Program.cs
@@ -0,0 +1,36 @@
+﻿
+var cities = new Dictionary<string, string>(){
+    {"TR", "Istanbul, Ankara, Izmir"},
+    {"UK", "London, Manchester, Birmingham"},
+    {"USA", "Chicago, New York, Washington"},
+    {"India", "Mumbai, New Delhi, Pune"}
+};
+
+System.Console.WriteLine($"*****Cities*****");
+foreach (KeyValuePair<string, string> city in cities)
+{
+    System.Console.WriteLine($"Company: {city.Key}, Cities: {city.Value}");
+}
+
+System.Console.WriteLine($"\n*****Contains*****");
+
+if (cities.ContainsKey("TR"))
+    System.Console.WriteLine($"TR Cities: {cities["TR"]}");
+
+System.Console.WriteLine($"\n*****TryGetValue*****");
+string result;
+if (cities.TryGetValue("USA", out result))
+    System.Console.WriteLine(result);
+
+System.Console.WriteLine($"\n*****Update*****");
+string country = "TR";
+cities[country] = "Istanbul, Ankara, Izmir, Antalya, Canakkale";
+System.Console.WriteLine($"Updated City: {cities[country]}");
+
+System.Console.WriteLine($"\n*****Remove*****");
+cities.Remove(country);
+
+foreach (KeyValuePair<string, string> city in cities)
+{
+    System.Console.WriteLine($"Company: {city.Key}, Cities: {city.Value}");
+}
